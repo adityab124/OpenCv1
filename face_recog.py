3bd494adaaa87365d2ac15c97c17d8ef545fed3d
Aditya Bhardwaj <34814396+adityab124@users.noreply.github.com>
2018-09-30 19:25:05 +0530

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -6,13 +6,10 @@ video_capture = cv2.VideoCapture(0)
 
 while True:
 
-    # Capture frame-by-frame
     retval, frame = video_capture.read()
 
-    # Convert to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect features specified in Haar Cascade
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -20,13 +17,10 @@ while True:
         minSize=(35, 35)
     )
 
-    # Draw a rectangle around recognized faces 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 200), 2)
 
-    # Display the resulting frame
     cv2.imshow('Video', frame)
 
-    # Exit the camera view
     if cv2.waitKey(1) & 0xFF == ord('q'):
        sys.exit() 
